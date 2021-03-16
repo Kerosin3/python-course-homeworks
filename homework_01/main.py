24e05d6a8edbea3ae84d057c3968661f3b32ca68
Kerosin3 <keromvp@gmail.com>
2021-03-16 13:51:43 +0300

    made prime_process global

--- homework_01/main.py
+++ homework_01/main.py
@@ -18,6 +18,20 @@ ODD = "odd"
 EVEN = "even"
 PRIME = "prime"
 
+def prime_process():
+    list_prime = []
+    for num in in_list:
+        if  num > 1 and isinstance(num, int): #check whether it greater than 1 
+            for i in range(2,num+1,1):
+                if num % i == 0: 
+                    if i != num: # if it is not the number itself then break
+                        break
+                    else: # if it is, then it is our prime number
+                        list_prime.append(num)
+                        continue
+        else:
+            continue # in case the number is 1 or not a number
+    return list_prime
 
 def filter_numbers(in_list,num_type):
     """
@@ -25,21 +39,6 @@ def filter_numbers(in_list,num_type):
     и возвращает только чётные/нечётные/простые числа
     (выбор производится передачей дополнительного аргумента)
     """
-    def prime_process():
-        list_prime = []
-        for num in in_list:
-            if  num > 1 and isinstance(num, int): #check whether it greater than 1 
-                for i in range(2,num+1,1):
-                    if num % i == 0: 
-                        if i != num: # if it is not the number itself then break
-                            break
-                        else: # if it is, then it is our prime number
-                            list_prime.append(num)
-                            continue
-            else:
-                continue # in case the number is 1 or not a number
-        return list_prime
- 
     if num_type == EVEN:
         return [item for item in in_list if (item % 2) == 0 and isinstance(item, int)]
     elif num_type == ODD:
