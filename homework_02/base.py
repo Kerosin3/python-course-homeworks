aa2c1b02bf332e3139b4c32d3aceac35259dd7d1
Kerosin3 <keromvp@gmail.com>
2021-03-31 22:25:50 +0300

    added HW02

---
+++ homework_02/base.py
@@ -0,0 +1,40 @@
+from abc import ABC
+from exceptions import LowFuelError,NotEnoughFuel,CargoOverload
+
+class Vehicle(ABC):
+    weight = None
+    started = None
+    fuel = 0.0
+    fuel_consumption = 0.0
+
+    def __init__(self,weight,fuel,fuel_consumption):
+        self.weight = weight
+        self.fuel = fuel
+        self.fuel_consumption = fuel_consumption
+
+    def __getattr__(self,sample):
+            print(f"getattr method has been called, param={sample}")
+        #return super().__getattr__(sample)
+    def start(self):
+        if self.started == 1:
+            print('we are already flying')
+
+        if not self.started:
+            if self.fuel >  0:
+                self.started = 1
+                print('стартууем!')
+            else:
+                raise LowFuelError('Zero fuel')
+            
+            
+    def move(self,distance:int):
+        if self.fuel < self.fuel_consumption * distance:
+            raise NotEnoughFuel()
+        print('distance confirmation')
+
+
+v0 = Vehicle(10,10500,2)
+v0.start()
+v0.start()
+v0.move(10)
+v0.password1 
---
+++ homework_02/exceptions.py
@@ -0,0 +1,15 @@
+"""
+Объявите следующие исключения:
+- LowFuelError
+- NotEnoughFuel
+- CargoOverload
+"""
+class LowFuelError(Exception):
+    pass
+
+
+class NotEnoughFuel(Exception):
+    pass
+
+class CargoOverload(Exception):
+    pass
