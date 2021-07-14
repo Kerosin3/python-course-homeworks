5f6b8e589396c75c99a4ab46cd7068403156406b
Kerosin3 <keromvp@gmail.com>
2021-07-14 17:15:52 +0300

    HW7 added base classes v2

--- hw7/django-app/django_app/stocks_app/stocks/models.py
+++ hw7/django-app/django_app/stocks_app/stocks/models.py
@@ -4,10 +4,11 @@ from datetime import datetime
 import time
 from collections import OrderedDict
 
+#current financials
 class Financials(models.Model):
-    pe = models.FloatField()
-    pb = models.FloatField()
-    mar_cap = models.FloatField()
+    pe = models.FloatField(default=0.0)
+    pb = models.FloatField(default=0.0)
+    mar_cap = models.FloatField(default=0.0)
 
 class Prices(models.Model):
     # prices = OrderedDict()
@@ -17,14 +18,17 @@ class Prices(models.Model):
 
 class Stock(models.Model):
     ticker = models.CharField(validators=[RegexValidator(regex='^.{4}$', message='Length has to be 4', code='nomatch')]
-                              ,unique=True)
-    current_price = models.DecimalField(default=0.0)
+                              ,unique=True,max_length=4)
+    current_price = models.DecimalField(max_digits=5,
+                                        decimal_places=2,
+                                        blank=True,
+                                        null=True,
+                                        default=0.0)
     created_at = models.DateTimeField(auto_now_add=True,null=True)
     updated_at = models.DateTimeField(auto_now=True,null=True)
     comment = models.TextField(blank=True)
-    financials = models.ForeignKey(Financials, on_delete=models.CASCADE)
     prices = models.ForeignKey(Prices, on_delete=models.CASCADE)
-
+    financials = models.ForeignKey(Financials, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.ticker
@@ -34,9 +38,11 @@ class Sector(models.Model):
     sector = models.CharField(max_length=20,unique=False)
     stock = models.OneToOneField(Stock,on_delete=models.PROTECT)
 
+class Misc():
 
-def get_cur_data():
-    #14/07/2021:15:23:09.930 format
-    time.sleep(0.05)
-    now = datetime.now()
-    return (now.strftime("%d/%m/%Y:%H:%M:%S.%f")[:-3])
+    @staticmethod
+    def get_cur_data():
+        #14/07/2021:15:23:09.930 format
+        time.sleep(0.05)
+        now = datetime.now()
+        return (now.strftime("%d/%m/%Y:%H:%M:%S.%f")[:-3])
---
+++ hw7/django-app/django_app/stocks_app/stocks/test_app.py
@@ -0,0 +1,5 @@
+# from .models import Stock,Sector,Financials
+#
+# if __name__ == '__main__':
+#     a = Stock.financials.pe
+#     print(a)
