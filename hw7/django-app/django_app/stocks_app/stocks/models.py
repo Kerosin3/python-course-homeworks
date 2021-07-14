474a65d92b2c58a72104a56a8e0372c8f32aae38
Kerosin3 <keromvp@gmail.com>
2021-07-14 17:27:45 +0300

    HW7 added base classes v3

--- hw7/django-app/django_app/stocks_app/stocks/models.py
+++ hw7/django-app/django_app/stocks_app/stocks/models.py
@@ -27,8 +27,8 @@ class Stock(models.Model):
     created_at = models.DateTimeField(auto_now_add=True,null=True)
     updated_at = models.DateTimeField(auto_now=True,null=True)
     comment = models.TextField(blank=True)
-    prices = models.ForeignKey(Prices, on_delete=models.CASCADE)
-    financials = models.ForeignKey(Financials, on_delete=models.CASCADE)
+    prices = models.ForeignKey(Prices, on_delete=models.CASCADE,blank=True, null=True)
+    financials = models.ForeignKey(Financials, on_delete=models.CASCADE,blank=True, null=True)
 
     def __str__(self):
         return self.ticker
@@ -36,7 +36,7 @@ class Stock(models.Model):
 class Sector(models.Model):
     #enumerate it
     sector = models.CharField(max_length=20,unique=False)
-    stock = models.OneToOneField(Stock,on_delete=models.PROTECT)
+    stock = models.OneToOneField(Stock,on_delete=models.PROTECT,blank=True, null=True)
 
 class Misc():
 
