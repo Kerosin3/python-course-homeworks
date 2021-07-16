fe4eb2d283c331c19ce344132061e0a75d18341b
Kerosin3 <keromvp@gmail.com>
2021-07-16 17:08:25 +0300

    added untasked tasks

--- hw7/django-app/django_app/stocks_app/stocks/views.py
+++ hw7/django-app/django_app/stocks_app/stocks/views.py
@@ -7,6 +7,10 @@ from .models import Stock
 def index_page(request):
     if request.method == 'GET':
         a_stock = Stock.objects.all()
-    else:
-        raise Http404
+    else: # POST
+        a_stock = Stock.objects.all()
+        with open('data_tickers.txt','w',encoding='utf-8') as filex:
+            for stock in a_stock:
+                filex.write(stock.ticker + '\n')
+        #raise Http404
     return render(request, 'stocks/index.html',{'stocks':a_stock})
